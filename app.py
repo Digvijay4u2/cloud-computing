19c3594b1074adda8ab2db43e85978c4b2d32a63
iamdhavalparmar <dkp48107@gmail.com>
2020-10-10 00:14:50 +0530

    final commit

--- app.py
+++ app.py
@@ -15,23 +15,16 @@ def predict():
     input_features = [float(x) for x in request.form.values()]
     features_value = [np.array(input_features)]
     
-    features_name = ['mean radius', 'mean texture', 'mean perimeter', 'mean area',
-       'mean smoothness', 'mean compactness', 'mean concavity',
-       'mean concave points', 'mean symmetry', 'mean fractal dimension',
-       'radius error', 'texture error', 'perimeter error', 'area error',
-       'smoothness error', 'compactness error', 'concavity error',
-       'concave points error', 'symmetry error', 'fractal dimension error',
-       'worst radius', 'worst texture', 'worst perimeter', 'worst area',
-       'worst smoothness', 'worst compactness', 'worst concavity',
-       'worst concave points', 'worst symmetry', 'worst fractal dimension']
+    features_name = ['num_preg', 'glucose_conc', 'diastolic_bp', 'thickness', 'insulin', 'bmi',
+             'diab_pred', 'age' , 'skin']
     
     df = pd.DataFrame(features_value, columns=features_name)
     output = model.predict(df)
         
     if output == 0:
-        res_val = "** breast cancer **"
+        res_val = "** Diabetes **"
     else:
-        res_val = "no breast cancer"
+        res_val = "No Diabetes"
         
 
     return render_template('index.html', prediction_text='Patient has {}'.format(res_val))
